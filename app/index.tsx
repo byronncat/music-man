863c6b0db2a11860cc6fbf5d42a51bbc243f6913
byronncat <anhthinhncat@gmail.com>
2024-10-09 15:13:54 +0700

    :wrench: Add fonts & Config ESLint and Prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,16 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    "DatDot-Bold": require("@assets/fonts/DatDot-Bold.otf"),
+    "Inter_18pt-Black": require("@assets/fonts/Inter_18pt-Black.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading fonts...</Text>;
+  }
+
   return (
     <View
       style={{
@@ -9,7 +19,17 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Home</Text>
+      <Text style={styles.fontDotdat}>Home</Text>
+      <Text style={styles.fontInter}>Text hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  fontInter: {
+    fontFamily: "Inter_18pt-Black",
+  },
+  fontDotdat: {
+    fontFamily: "DatDot-Bold",
+  },
+});
---
+++ constants/ui.constant.ts
@@ -0,0 +1,15 @@
+export const Typography = {
+  primary: "Inter-Regular",
+  thin: "Inter-Thin",
+  extraLight: "Inter-ExtraLight",
+  light: "Inter-Light",
+  medium: "Inter-Medium",
+  semiBold: "Inter-SemiBold",
+  bold: "Inter-Bold",
+  extraBold: "Inter-ExtraBold",
+  black: "Inter-Black",
+};
+
+export const Color = {
+  primary: "#FF0000",
+};
